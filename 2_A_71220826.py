10c7ca32d64c0cb5919158c113d4a85faf1ee22e
darkoboy <118077486+dewaahr@users.noreply.github.com>
2022-12-02 10:27:05 +0700

    Update 2_A_71220826.py

--- 2_A_71220826.py
+++ 2_A_71220826.py
@@ -1,7 +1,7 @@
 def cetakHuruf(str):
     jumlah=len(str)
     if jumlah % 2==0:
-        return str[0]+str[1]+str[2]+"dan"+str[-3]+str[-2]+str[-1]+str[-]
+        return str[0]+str[1]+str[2]+"dan"+str[-3]+str[-2]+str[-1]
     else:
         tengah=int(jumlah/2)
         return str[tengah-1]+str[tengah]+str[tengah+1]
